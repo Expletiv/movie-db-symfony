4fea79d4c76c00e018334dd8a7a0f88b9d4efb91
Fabian Friedrich <fabian.friedrich@check24.de>
2024-06-09 18:06:02 +0200

    ADD frontend homepage with bootstrap

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,11 +8,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/')]
     #[Route('/admin')]
     public function homepage(): RedirectResponse
     {
