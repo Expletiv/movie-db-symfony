649e2119b9d5d06020cafb4c66a68aa50a67c6cc
Fabian Friedrich <fabian.friedrich@check24.de>
2024-07-10 22:11:22 +0200

    ADD theme switch functionality

---
+++ assets/controllers/theme_controller.ts
@@ -0,0 +1,28 @@
+import {Controller} from '@hotwired/stimulus';
+
+export default class extends Controller {
+
+  static targets = ['themeSwitch']
+
+  private DEFAULT_THEME = 'dark';
+
+  connect() {
+    let theme = localStorage.getItem('preferred_theme');
+    this.updateTheme(theme);
+  }
+
+  changeTheme() {
+    let theme = localStorage.getItem('preferred_theme');
+    theme = theme === 'dark' ? 'light' : 'dark';
+    this.updateTheme(theme);
+  }
+
+  private updateTheme(theme: string | null) {
+    if (theme === null) {
+      theme = this.DEFAULT_THEME;
+    }
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('preferred_theme', theme);
+    this.themeSwitchTarget.checked = theme === 'dark';
+  }
+}
