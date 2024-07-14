bfdb5dc460083dbcabb1583aeaf2bb7051cc6162
Fabian Friedrich <fabian.friedrich@check24.de>
2024-07-14 17:32:30 +0200

    CHANGE handling of missing frames on redirect
    Found a better solution that supports only getting redirected when the frame is actually missing.

--- assets/controllers/helper_controller.ts
+++ assets/controllers/helper_controller.ts
@@ -1,36 +1,26 @@
 import {Controller} from "@hotwired/stimulus";
-import {FrameElement, TurboBeforeFetchResponseEvent, visit} from '@hotwired/turbo';
+import {TurboFrameMissingEvent} from "@hotwired/turbo";
 
 export default class extends Controller {
 
   connect() {
-    document.addEventListener('turbo:before-fetch-response', this.beforeFetchResponse);
+    document.addEventListener('turbo:frame-missing', this.onFrameMissing);
   }
 
   disconnect() {
-    document.removeEventListener('turbo:before-fetch-response', this.beforeFetchResponse)
+    document.removeEventListener('turbo:frame-missing', this.onFrameMissing)
   }
 
   linkToDetailsPage(event: { params: { url: string; }; }) {
-    visit(event.params.url);
+    Turbo.visit(event.params.url);
   }
 
-  // see here: https://symfonycasts.com/screencast/turbo/full-frame-redirect
-  beforeFetchResponse(event: TurboBeforeFetchResponseEvent) {
-    const fetchResponse = event.detail.fetchResponse;
-    // @ts-ignore
-    if (!fetchResponse.succeeded || !fetchResponse.redirected) {
-      return;
+  onFrameMissing(event: TurboFrameMissingEvent) {
+    const response: Response = event.detail.response;
+    if (response.ok && response.redirected) {
+      event.detail.visit(response.url, {action: "advance", frame: undefined});
+      event.preventDefault();
     }
-    // @ts-ignore
-    const frame: ?FrameElement = event.target?.closest('turbo-frame');
-    // Add data-turbo-form-redirect to the turbo-frame if you want it to redirect the page
-    if (!frame || !frame.dataset.turboFormRedirect) {
-      return;
-    }
-
-    event.preventDefault();
-    Turbo.visit(fetchResponse.location.toString());
   }
 
 }
