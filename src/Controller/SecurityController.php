661ea3ae8b976af07c74b4491472e460cada84ba
Fabian Friedrich <fabian.friedrich@check24.de>
2024-07-14 20:14:58 +0200

    ADD access control for admin page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -30,4 +30,16 @@ class SecurityController extends AbstractController
     {
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/login/error', name: 'app_login_error')]
+    public function loginError(): Response
+    {
+        return $this->render('security/login_error.html.twig');
+    }
+
+    #[Route(path: '/login/access-denied', name: 'app_login_access_denied')]
+    public function loginAccessDenied(): Response
+    {
+        return $this->render('security/login_access_denied.html.twig');
+    }
 }
