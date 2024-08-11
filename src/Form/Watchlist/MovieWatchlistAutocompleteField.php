3069524a87abe23fa7f24f1960891880f251a6af
Expletiv <fabian.friedrich03@gmail.com>
2024-08-11 12:13:37 +0200

    ADD security to autocomplete endpoint

--- src/Form/Watchlist/MovieWatchlistAutocompleteField.php
+++ src/Form/Watchlist/MovieWatchlistAutocompleteField.php
@@ -3,6 +3,8 @@
 namespace App\Form\Watchlist;
 
 use App\Entity\MovieWatchlist;
+use App\Services\UserProvider;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\Autocomplete\Form\AsEntityAutocompleteField;
@@ -11,12 +13,23 @@ use Symfony\UX\Autocomplete\Form\BaseEntityAutocompleteType;
 #[AsEntityAutocompleteField]
 class MovieWatchlistAutocompleteField extends AbstractType
 {
+    public function __construct(
+        private readonly UserProvider $userProvider,
+    ) {
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'class' => MovieWatchlist::class,
             'placeholder' => 'forms.autocomplete.watchlist.placeholder',
-            'searchable_fields' => ['name'],
+            'filter_query' => function (QueryBuilder $qb, string $query) {
+                $qb->andWhere('entity.name LIKE :query')
+                    ->andWhere('entity.owner = :owner')
+                    ->setParameter('query', '%'.$query.'%')
+                    ->setParameter('owner', $this->userProvider->authenticateUser()->getId());
+            },
+            'security' => 'IS_AUTHENTICATED',
             'multiple' => true,
         ]);
     }
