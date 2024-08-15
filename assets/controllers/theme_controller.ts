f714ed9ebd335c48851d1880f0390c75003cc192
Expletiv <fabian.friedrich03@gmail.com>
2024-08-15 20:08:24 +0200

    Use icon as theme switch

--- assets/controllers/theme_controller.ts
+++ assets/controllers/theme_controller.ts
@@ -2,11 +2,13 @@ import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
 
-  static targets = ['themeSwitch']
+  static targets = ['themeSwitch', 'themeIcon']
 
-  declare readonly themeSwitchTarget: HTMLInputElement;
+  declare readonly themeIconTarget: HTMLLabelElement;
 
   private DEFAULT_THEME = 'dark';
+  private ICON_LIGHT = ['bi-sun-fill', 'text-warning'];
+  private ICON_DARK = ['bi-moon-stars-fill'];
 
   connect() {
     let theme = localStorage.getItem('preferred_theme');
@@ -24,7 +26,10 @@ export default class extends Controller {
       theme = this.DEFAULT_THEME;
     }
     document.documentElement.setAttribute('data-bs-theme', theme);
+
+    this.themeIconTarget.classList.remove(...(theme === 'dark' ? this.ICON_LIGHT : this.ICON_DARK));
+    this.themeIconTarget.classList.add(...(theme === 'dark' ? this.ICON_DARK : this.ICON_LIGHT));
+
     localStorage.setItem('preferred_theme', theme);
-    this.themeSwitchTarget.checked = theme === 'dark';
   }
 }
